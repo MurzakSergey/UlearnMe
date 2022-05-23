cea1ef9e781c9d1dd1f6635c4c6c93ab0f0453f2
MurzakSergey <murzaksergey@gmail.com>
2022-05-23 23:47:37 +0300

    first commit

---
+++ Methods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+
+        public static string GetGreetingMessage(string name, double salary)
+        {
+         salary++;
+         return name + salary;
+
+        }
+        static void Main(string[] args)
+        {
+            string Lola = "Lola";
+            double millioni = 1;
+            GetGreetingMessage(Lola, millioni);
+            Console.WriteLine(GetGreetingMessage(Lola, millioni));
+            Console.ReadKey();
+        }
+
+       
+    }
+}
