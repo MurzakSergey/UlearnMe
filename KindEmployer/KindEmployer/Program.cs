b15f2b60cf85c0f52f7efbcad733be3b4b0fa85d
MurzakSergey <murzaksergey@gmail.com>
2022-05-27 23:48:36 +0300

    what about exeptions?

---
+++ KindEmployer/KindEmployer/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KindEmployer
+{
+    internal class Program
+    {
+        public static string GetGreetingMessage(string name, double salary)
+        {
+            salary = (int)Math.Ceiling(salary);
+            return name + salary;
+           
+        }
+
+        public static string GetSquare(double theNumber)
+        {
+            string fourtyTwo = theNumber.ToString();
+            return "The main answer is " + fourtyTwo;
+            
+        }
+        static void Main(string[] args)
+        {
+            string Lola = "Lola";
+            double millioni = 999999999.000001;
+            //GetGreetingMessage(Lola, millioni);
+
+            int odin = 1;
+            string Histch = "n" + odin.ToString();
+
+            double unswer2 = Math.Pow(5, 2);
+
+            Console.WriteLine(GetGreetingMessage(Lola, millioni));
+            Console.WriteLine(Histch);
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ searchingMethods/searchingMethods/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace searchingMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetLastHalf("I love CSharp!"));
+            Console.WriteLine(GetLastHalf("1234567890"));
+            Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+            Console.ReadLine();
+                     
+        }
+
+        public static string GetLastHalf(string text)
+
+        {
+            int length = text.Length / 2;
+            text = text.Substring(length);
+            text = text.Replace(" ", "");
+            return text;
+
+            //return text.Substring(text.Length / 2).Replace(" ", "");  Ответ преподавателя
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
